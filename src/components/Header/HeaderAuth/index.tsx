647dbdf86e2165bcb666ac80fe31368eedcd9b80
Mike <mkeacs@gmail.com>
2023-08-24 17:41:41 -0300

    adding some changes

---
+++ src/components/Header/HeaderAuth/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./HeaderAuth.module.css";
+
+export const HeaderAuth = () => {
+  return (
+    <>
+      <nav className={styles.nav}>
+        <ul>
+          <li>Centro de segurança</li>
+        </ul>
+      </nav>
+    </>
+  );
+};
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { HeaderAuth } from "./HeaderAuth";
+import { HeaderEditProfile } from "./HeaderEditProfile";
+import { HeaderProfile } from "./HeaderProfile";
+import { HeaderRoot } from "./HeaderRoot";
+
+export const Header = {
+  Root: HeaderRoot,
+  Auth: HeaderAuth,
+  Profile: HeaderProfile,
+  Edit: HeaderEditProfile,
+};
