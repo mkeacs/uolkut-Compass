5048803d4955070b5adb7a7d5b538ac1024005f1
Mike <mkeacs@gmail.com>
2023-08-26 16:35:23 -0300

    adding auth login/register

--- src/components/Form/FormLogin/index.tsx
+++ src/components/Form/FormLogin/index.tsx
@@ -1,22 +1,57 @@
+import React, { useState } from "react";
 import { Form, Link, useNavigate } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "../../../services/firebaseConfig";
 import styles from "./FormLogin.module.css";
 
 export const FormLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleFormSubmit = (event: React.SyntheticEvent) => {
+  const [signInWithEmailAndPassword, user, loading] =
+    useSignInWithEmailAndPassword(auth);
+
+  const handleFormSubmit = async (event: React.SyntheticEvent) => {
     event.preventDefault();
-    navigate("profile");
+
+    try {
+      await signInWithEmailAndPassword(email, password);
+
+      navigate("profile");
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
+    }
   };
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  if (user) {
+    console.log("Usuário logado:", user);
+  }
 
   return (
     <Form onSubmit={handleFormSubmit} className={styles.formLoginContainer}>
       <fieldset>
         <p>
-          <input type="email" placeholder="E-mail" required />
+          <input
+            type="email"
+            placeholder="E-mail"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </p>
         <p>
-          <input type="password" placeholder="Senha" required />
+          <input
+            type="password"
+            placeholder="Senha"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </p>
         <p className={styles.checkboxContainer}>
           <input type="checkbox" name="remember-password" />
@@ -25,7 +60,9 @@ export const FormLogin = () => {
       </fieldset>
 
       <fieldset>
-        <button className={styles.button_login}>Entrar na conta</button>
+        <button type="submit" className={styles.button_login}>
+          Entrar na conta
+        </button>
 
         <Link to="/register">
           <button className={styles.button_register}>Criar uma conta</button>
---
+++ src/services/firebase.ts
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyCZffeE0wrpop29y3jix_A4xxm5QgzrhLY",
+  authDomain: "uolkut-c23e6.firebaseapp.com",
+  projectId: "uolkut-c23e6",
+});
+
+export default firebaseApp;
